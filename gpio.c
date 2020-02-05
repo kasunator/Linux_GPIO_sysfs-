0845a226e43351f81cd24d7784d5fb74031ba23a
Kasun Beddewela <kbeddewela@aampglobal.com>
2020-02-05 11:53:22 -0500

    wrote mass intit fucntions to init the gpions before testing

--- gpio.c
+++ gpio.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<unistd.h>
 #define DEBUG
 #define AC_AUTO_LED_PATH  "/sys/class/leds/ac_auto/brightness"
 #define GPIO_PIN_EXPORT_PATH	"/sys/class/gpio/export"
@@ -12,12 +13,12 @@
 #define GPIO_PIN_BANK_C_SUB_STRING "/sys/class/gpio/PC"
 #define GPIO_PIN_BANK_D_SUB_STRING "/sys/class/gpio/PD"
 
-static int exportPin(int pin_num);
-static int unexportPin(int pin_num);
-static int set_GPIO_as_output(int pin_num);
-static int set_GPIO_as_input(int pin_num);
-static int read_GPIO_state(int pin_num);
-static int write_GPIO_output_state(int pin_num, int value);
+ int exportPin(int pin_num);
+ int unexportPin(int pin_num);
+ int set_GPIO_as_output(int pin_num);
+ int set_GPIO_as_input(int pin_num);
+ int read_GPIO_state(int pin_num);
+ int write_GPIO_output_state(int pin_num, int value);
 
 
 
@@ -33,9 +34,9 @@ int main() {
 	} else {
 		/* this function reads 5 characters,just a random long enough number,
 		 read the length or till new line or endof file is met, what ever comes first */
-		ret = fgets(state,5,fp); 
+		ret = fgets(state,5,fp);
 		if (ret == NULL) {
-                	printf("the read failed\n");		
+                	printf("the read failed\n");
 		} else {
 		 	printf("The file string %s \n", state );
                         printf("read length %d \n", strlen(state) ); /*this return 2*/
@@ -57,28 +58,74 @@ int main() {
 	}
 	return 0;
 }
-#endif 
+#endif
 
+//#if 0
 int main() {
-       int ret;
+  int ret;
 	printf("exporting pin\n");
 	exportPin(31);
-	set_GPIO_as_output(31);
-        ret =read_GPIO_state(31);
-        if (ret == 1 ) {
-		printf("ret was 1 ");
-          	write_GPIO_output_state(31,0);
+	set_GPIO_as_output(31);//this will aotmatically reset the gpio ouput state to 0
+	sleep(1);
+  ret =read_GPIO_state(31);
+	printf("ret was: %d\n", ret);
+  if (ret == 1 ) {//so this condition will never be set
+    write_GPIO_output_state(31,0);
 	} else {
-		printf("ret was 0 ");
 		write_GPIO_output_state(31,1);
 	}
 }
+//#endif
+/*
+    int main(int argc, char *argv[]) {  }
+    argc (ARGument Count) is int and stores number of command-line arguments passed by the user including the name of the program.
+		So if we pass a value to a program, value of argc would be 2 (one for argument and one for program name)
+    The value of argc should be non negative.
+    argv(ARGument Vector) is array of character pointers listing all the arguments.
+    If argc is greater than zero,the array elements from argv[0] to argv[argc-1] will contain pointers to strings.
+    Argv[0] is the name of the program , After that till argv[argc-1] every element is command -line arguments.
 
-static int set_GPIO_as_output(int pin_num) {
+		we can call the ./executable_name arg1 arg2 arg3
+
+*/
+#if 0
+int main(int argc, char *argv[]) {
+
+	if ( argc  > 0) {
+		if( strncmp(argv[1],"write",5) == 0 ) {
+			printf("command 1 :write \n");
+			if( strncmp(argv[2],"1",1) == 0) {
+				printf("parm 1: 1\n");
+				write_GPIO_output_state(31,1);
+			} else if ( strncmp(argv[2],"1",0) == 0 ) {
+				printf("parm 1: 0\n");
+				write_GPIO_output_state(31,0);
+			}
+		} else if ( strncmp(argv[1],"read",4) == 0) {
+			printf("command 1 :read \n");
+			read_GPIO_state(31);
+		} else if ( strncmp(argv[1],"toggle",6) == 0 ) {
+			printf("command 1 :toggle \n");
+			int ret =read_GPIO_state(31);
+			printf("read_GPIO_state returned : %d",ret);
+			if (ret == 1 ) {
+				printf("ret was 1 ");
+				write_GPIO_output_state(31,0);
+			} else {
+				printf("ret was 0\n");
+				write_GPIO_output_state(31,1);
+			}
+		}
+	}
+}
+#endif
+
+int set_GPIO_as_output(int pin_num) {
 	FILE* fp = NULL;
+	int ret_value;
 	char gpio_pin_file_string[strlen(GPIO_PIN_BANK_A_SUB_STRING)  + strlen("/direction")+2 ] ;
- 
-      	if (pin_num < 32) {
+
+  if (pin_num < 32) {
   		sprintf(gpio_pin_file_string,GPIO_PIN_BANK_A_SUB_STRING "%d/direction",pin_num);
 	} else if(pin_num < 64) {
 		sprintf(gpio_pin_file_string,GPIO_PIN_BANK_B_SUB_STRING "%d/direction",pin_num);
@@ -92,20 +139,28 @@ static int set_GPIO_as_output(int pin_num) {
 
 #endif
 	fp = fopen(gpio_pin_file_string,"w");
-        if (fp != NULL) {
-		fprintf(fp,"out");
-		return 0;
+  if (fp != NULL) {
+		ret_value = fprintf(fp,"out"); //On success, the total number of characters written is returned.
+		printf("ret_value: %d\n",ret_value);
+		if ( ret_value > 0 ) {
+			  fclose(fp);
+				return 0;
+		} else {
+				fclose(fp);
+				return -1;
+		}
 	} else {
 		printf("faile openning file:[%s]\n",gpio_pin_file_string);
 		return -1;
 	}
 }
 
-static int set_GPIO_as_input(int pin_num) {
+int set_GPIO_as_input(int pin_num) {
 	FILE* fp = NULL;
+	int ret_value;
 	char gpio_pin_file_string[strlen(GPIO_PIN_BANK_A_SUB_STRING) + strlen("/direction")+2 ] ;
- 
-      	if (pin_num < 32) {
+
+  if (pin_num < 32) {
   		sprintf(gpio_pin_file_string,GPIO_PIN_BANK_A_SUB_STRING "%d/direction",pin_num);
 	} else if(pin_num < 64) {
 		sprintf(gpio_pin_file_string,GPIO_PIN_BANK_B_SUB_STRING "%d/direction",pin_num);
@@ -117,10 +172,15 @@ static int set_GPIO_as_input(int pin_num) {
 #ifdef DEBUG
 	printf("DEBUG gpio_pin_file_string:[%s]\n",gpio_pin_file_string);
 
-#endif	
+#endif
 	fp = fopen(gpio_pin_file_string,"w");
-        if (fp != NULL) {
-		fprintf(fp,"in");
+  if (fp != NULL) {
+		ret_value = fprintf(fp,"in");
+		if ( ret_value < 0) {
+			fclose(fp);
+		} else {
+			return -1;
+		}
 		return 0;
 	} else {
 		printf("faile openning file:[%s]\n",gpio_pin_file_string);
@@ -128,12 +188,12 @@ static int set_GPIO_as_input(int pin_num) {
 	}
 }
 
-static int read_GPIO_state(int pin_num) {
+int read_GPIO_state(int pin_num) {
 	FILE* fp = NULL;
-	char state[2] = {0};
+	char state[3] = {0};
 	char gpio_pin_file_string[strlen(GPIO_PIN_BANK_A_SUB_STRING)+ strlen("/value") +2] ;
-        char* ret;
-      	if (pin_num < 32) {
+  char* ret;
+  if (pin_num < 32) {
   		sprintf(gpio_pin_file_string,GPIO_PIN_BANK_A_SUB_STRING "%d/value",pin_num);
 	} else if (pin_num < 64) {
 		sprintf(gpio_pin_file_string,GPIO_PIN_BANK_B_SUB_STRING "%d/value",pin_num);
@@ -145,14 +205,22 @@ static int read_GPIO_state(int pin_num) {
 #ifdef DEBUG
 	printf("DEBUG gpio_pin_file_string :[%s]\n",gpio_pin_file_string);
 
-#endif	
+#endif
 
 	fp = fopen(gpio_pin_file_string,"r");
-        if (fp != NULL) {
-		ret = fgets(state,2,fp);
-                if (ret != NULL ) {
-			return state[0];      
-		} else { 
+  if (fp != NULL) {
+		ret = fgets(state,3,fp);
+		printf("read state: %s \n",state);
+    if (ret != NULL ) {
+			fclose(fp);
+			if(state[0] == '1') {
+				return 1;
+			} else if ( state[0] == '0') {
+				return 0;
+			} else {
+				return state[0];
+			}
+		} else {
 			return -1;
 		}
 	} else {
@@ -162,14 +230,14 @@ static int read_GPIO_state(int pin_num) {
 
 }
 
-static int write_GPIO_output_state(int pin_num, int value){
+int write_GPIO_output_state(int pin_num, int value){
 	FILE* fp = NULL;
 	char state[2] = {0};
 	char gpio_pin_file_string[strlen(GPIO_PIN_BANK_A_SUB_STRING)+ strlen("/value") +2];
-        char write_value;
+  char write_value;
 	int ret;
-      	if (pin_num < 32) {
-  		sprintf(gpio_pin_file_string,GPIO_PIN_BANK_A_SUB_STRING "%d/value",pin_num);
+  if (pin_num < 32) {
+  	sprintf(gpio_pin_file_string,GPIO_PIN_BANK_A_SUB_STRING "%d/value",pin_num);
 	} else if (pin_num < 64) {
 		sprintf(gpio_pin_file_string,GPIO_PIN_BANK_B_SUB_STRING "%d/value",pin_num);
 	} else if (pin_num < 98) {
@@ -180,16 +248,23 @@ static int write_GPIO_output_state(int pin_num, int value){
 #ifdef DEBUG
 	printf("DEBUG gpio_pin_file_string :[%s]\n",gpio_pin_file_string);
 
-#endif	
+#endif
 
-	fp = fopen(gpio_pin_file_string,"w");
-        if (fp != NULL) {
+	fp = fopen(gpio_pin_file_string,"w+");
+  if (fp != NULL) {
 		if(value ==0 )
 			write_value = '0';
 		else
-			write_value = '1';	
+			write_value = '1';
+		printf("write value = %c \n", write_value);
 		ret = fprintf(fp,"%c\n",write_value);
-		return ret;
+		if ( ret < 0 ) {
+			printf("failed writing: [%c] \n", write_value );
+			return -1;
+		} else {
+			fclose(fp);
+			return ret;
+		}
 	} else {
 		printf("faile openning file:[%s]\n",gpio_pin_file_string);
 		return -1;
@@ -197,10 +272,10 @@ static int write_GPIO_output_state(int pin_num, int value){
 }
 
 
-static int exportPin(int pin_num) {
+int exportPin(int pin_num) {
 	FILE* fp = NULL;
-	
-         
+
+
         fp = fopen(GPIO_PIN_EXPORT_PATH,"w");
 	if ( fp != NULL) {//to make sure we didnt have a problem re opening the same file
 		fprintf(fp,"%d\n", pin_num);
@@ -212,11 +287,11 @@ static int exportPin(int pin_num) {
 	}
 }
 
-static int unexportPin(int pin_num) {
+int unexportPin(int pin_num) {
 	FILE* fp = NULL;
-	
-         
-        fp = fopen(GPIO_PIN_UNEXPORT_PATH,"w");
+
+
+  fp = fopen(GPIO_PIN_UNEXPORT_PATH,"w");
 	if ( fp != NULL) {//to make sure we didnt have a problem re opening the same file
 		fprintf(fp,"%d\n", pin_num);
 		fclose(fp);
@@ -226,5 +301,3 @@ static int unexportPin(int pin_num) {
 		return -1;
 	}
 }
-
-
--- validation_test.c
+++ validation_test.c
@@ -4,82 +4,237 @@
 #include "gpio.h"
 
 
-int gpio_bank_A_Output_netlist[15] = {
-					0x1,
-					0x2,
-					0x8,
-					0x10,
-					0x20,
-					0x40,
-					0x80,
-					0x100,
-					0x200,
-					0x400,
-					0x800,
-					0x1000,
-					0x2000,
-					0x4000,
-					0x8000 };
+unsigned int gpio_bank_A_Output_netlist[15] = {
+	0x1,
+	0x2,
+	0x8,
+	0x10,
+	0x20,
+	0x40,
+	0x80,
+	0x100,
+	0x200,
+	0x400,
+	0x800,
+	0x1000,
+	0x2000,
+	0x4000,
+	0x8000 };
 
-int gpio_bank_A_Input_netlist[15] = {
-					0x80000000,
-					0x40000004,
-					0x10000000,
-					0x8000000,
-					0x4000000,
-					0x2000000,
-					0x1000000,
-					0x800000,
-					0x400000,
-					0x200000,
-					0x100000,
-					0x80000,
-					0x40000,
-					0x20000,
-					0x10000};
+unsigned int gpio_bank_A_Input_netlist[15] = {
+	0x80000000,
+	0x40000004,
+	0x10000000,
+	0x8000000,
+	0x4000000,
+	0x2000000,
+	0x1000000,
+	0x800000,
+	0x400000,
+	0x200000,
+	0x100000,
+	0x80000,
+	0x40000,
+	0x20000,
+	0x10000};
+
+unsigned int  gpio_bank_B_Output_netlist = {
+	0x1,
+	0x2,
+	0x4,
+	0x8,
+	0x10,
+	0x800,
+	0x1000,
+	0x2000,
+	0x4000,
+	0x8000,
+	0x10000,
+	0x20000,
+	0x40000};
+
+unsigned int  gpio_bank_B_Input_netlist = {
+	0x80000000,
+	0x40000000,
+	0x20000000,
+	0x10000000,
+	0x8000000,
+	0x4000000,
+	0x2000000,
+	0x1000000,
+	0x800000,
+	0x400000,
+	0x200000,
+	0x100000,
+	0x80000};
+
+unsigned int  gpio_bank_C_Output_netlist = {
+	0x1,
+	0x2,
+	0x4,
+	0x8,
+	0x10,
+	0x20,
+	0x40,
+	0x80,
+	0x100,
+	0x200,
+	0x400,
+	0x800,
+	0x1000,
+	0x2000,
+	0x4000,
+	0x8000};
+
+unsigned int  gpio_bank_C_Input_netlist = {
+	0x80000000,
+	0x40000000,
+	0x20000000,
+	0x10000000,
+	0x8000000,
+	0x4000000,
+	0x2000000,
+	0x1000000,
+	0x800000,
+	0x400000,
+	0x200000,
+	0x100000,
+	0x80000,
+	0x40000,
+	0x20000,
+	0x10000};
+
+unsigned int  gpio_bank_D_Output_netlist = {
+	0x1,
+	0x2,
+	0x4,
+	0x8,
+	0x10,
+	0x20,
+	0x40};
+
+unsigned int   gpio_bank_D_Input_netlist = {
+	0x10000,
+	0x8000,
+	0x4000,
+	0x2000,
+	0x1000,
+	0x100,
+	0x80};
+
+enum enum_pin_direction{
+	DIRECTION_INPUT=0,
+	DIRECTION_OUTPUT
+	};
+enum GPIO_bank{
+	BANK_A =0,
+	BANK_B,
+	BANK_C,
+	BANL_D
+};
+
+static int _init_pins(enum GPIO_bank bank ,
+	unsigned int netlist[],int list_len, enum enum_pin_direction direction  ){
+
+	int i =0, j =0,func_ret =0, pin_number;
 
 
-int static _init_gpio_A_pins(){
-			 	int outPutListLenght = sizeof(gpio_bank_A_Output_netlit) ;
-			 	int inPutListLength = sizeof(gpio_bank_A_Input_netlist);
-			 	int i =0, j =0, bit_count =0;
-				int func_ret =0,
-					//first configure the output pins
-				for ( i =0 ; i < outPutListLength ; i++ ) {
-					//export the outputs
-					for( j =0 ; j < 32 ; j++) {
-						if ( ((gpio_bank_A_Output_netlist[i]>>j) & 0x01) ) {//check it the jth bit is set
-							func_ret = exportPin(j);
-							if ( func_ret > 0) {
-								func_ret = set_GPIO_as_output(j);
-								if ( func_ret < 0 ) {
-									printf("error setting setting output pin as output")
-								}
-							} else {
-								printf("error exporting output pin");
-							}
-						}
-					}
+	for ( i =0 ; i < list_len ; i++ ) {
+		//export netlist
+		for( j =0 ; j < 32 ; j++) {
+			if ( (netlist[i]>>j) & 0x01 ) {//check it the jth bit is set
+				pin_number = j+(bank*32);
+				func_ret = exportPin(pin_number);
+				if ( func_ret > 0) {
+					if (direction == DIRECTION_INPUT)
+						func_ret = set_GPIO_as_input(pin_number);
+				 	else if (direction == DIRECTION_OUTPUT)
+						func_ret = set_GPIO_as_output(pin_number);
+					if ( func_ret < 0 )
+						printf("error setting setting output pin as output")
+						return -1;
+
+				} else {
+					printf("error exporting output pin");
+					return -1;
 				}
-				//now configure the input pins
-				for ( i =0 ; i < inPutListLength ; i++ ) {
-					//export the outputs
-					for( j =0 ; j < 32 ; j++) {
-						if ( ((gpio_bank_A_Output_netlist[i]>>j) & 0x01) ) {//check it the jth bit is set
-							func_ret = exportPin(j);
-							if ( func_ret > 0) {
-								func_ret = set_GPIO_as_input(j);
-								if ( func_ret < 0 ) {
-									printf("error setting setting output pin as output")
-								}
-							} else {
-								printf("error exporting output pin");
-							}
-						}
-					}
-				}
+			}
+		}
+	}
+
 }
 
+static int init_all_pins(){
+	//init bank A inputs
+	_init_pins(BANK_A, gpio_bank_A_Input_netlist, sizeof(gpio_bank_A_Input_netlist), DIRECTION_INPUT );
+	//init bank A outputs
+	_init_pins(BANK_A, gpio_bank_A_Output_netlist, sizeof(gpio_bank_A_Output_netlist), DIRECTION_OUTPUT );
+
+	//init bank B inputs
+	_init_pins(BANK_B, gpio_bank_B_Input_netlist, sizeof(gpio_bank_B_Input_netlist), DIRECTION_INPUT );
+	//init bank B outputs
+	_init_pins(BANK_B, gpio_bank_B_Output_netlist, sizeof(gpio_bank_B_Output_netlist), DIRECTION_OUTPUT );
+
+	//init bank C inputs
+	_init_pins(BANK_C, gpio_bank_C_Input_netlist, sizeof(gpio_bank_C_Input_netlist), DIRECTION_INPUT );
+	//init bank C outputs
+	_init_pins(BANK_C, gpio_bank_C_Output_netlist, sizeof(gpio_bank_C_Output_netlist), DIRECTION_OUTPUT );
+
+	//init bank D inputs
+	_init_pins(BANK_D, gpio_bank_D_Input_netlist, sizeof(gpio_bank_D_Input_netlist), DIRECTION_INPUT );
+	//init bank D outputs
+	_init_pins(BANK_D, gpio_bank_D_Output_netlist, sizeof(gpio_bank_D_Output_netlist), DIRECTION_OUTPUT );
+
+}
+
+
+
+
+/*
+
+int static _init_gpio_A_pins(){
+
+ 	int outPutListLenght = sizeof(gpio_bank_A_Output_netlit) ;
+ 	int inPutListLength = sizeof(gpio_bank_A_Input_netlist);
+ 	int i =0, j =0, bit_count =0,func_ret =0, pin_number;
+
+		//first configure the output pins
+	for ( i =0 ; i < outPutListLength ; i++ ) {
+		//export the outputs
+		for( j =0 ; j < 32 ; j++) {
+			if ( (gpio_bank_A_Output_netlist[i]>>j) & 0x01 ) {//check it the jth bit is set
+				pin_number = j;
+				func_ret = exportPin(pin_number);
+				if ( func_ret > 0) {
+					func_ret = set_GPIO_as_output(pin_number);
+					if ( func_ret < 0 ) {
+						printf("error setting setting output pin as output")
+					}
+				} else {
+					printf("error exporting output pin");
+				}
+			}
+		}
+	}
+	//now configure the input pins
+	for ( i =0 ; i < inPutListLength ; i++ ) {
+		//export the outputs
+		for( j =0 ; j < 32 ; j++) {
+			if ( ((gpio_bank_A_Output_netlist[i]>>j) & 0x01) ) {//check it the jth bit is set
+				func_ret = exportPin(j);
+				if ( func_ret > 0) {
+					func_ret = set_GPIO_as_input(j);
+					if ( func_ret < 0 ) {
+						printf("error setting setting output pin as output")
+					}
+				} else {
+					printf("error exporting output pin");
+				}
+			}
+		}
+	}
+}
+*/
 
 
 
